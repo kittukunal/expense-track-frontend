ba970cbcae69defaa16c6e0fabc7b4234e420bc9
Kunal Yadav <158393864+kittukunal@users.noreply.github.com>
2025-05-05 00:13:09 +0530

    Update endpoints.js

--- src/features/api/endpoints.js
+++ src/features/api/endpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://your-backend.onrender.com/api/v1"
+export const BASE_URL = "https://expense-track-backends.onrender.com/api/v1"
 export const USERS_URL = "/users";
 export const INCOMES_URL = "/incomes";
 export const EXPENSES_URL = "/expenses";
